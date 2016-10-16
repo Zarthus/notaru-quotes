8f4e51f58797be7d78facfd7eb2db4c929804a75
Jos Ahrens <buughost@gmail.com>
2016-10-16 23:31:15 +0000

    implement the following get requests: author, quote, quotes, search
    Closes #4
    
    Closes #3
    
    Closes #2

--- out/script.js
+++ out/script.js
@@ -20,7 +20,8 @@
     split = item.split("=");
     key = split[0];
     value = split[1];
-    for (var x = 0, k = keys[x]; x < keys.length; x++) {
+    for (var x = 0, k; x < keys.length; x++) {
+      k = keys[x];
       if (k == key) {
         listeners[k] = decodeURIComponent(value);
       }
@@ -29,17 +30,23 @@
   handleGetRequest(listeners);
 })();
 
-function quoteToggleShown(quoteId, quoteClassName) {
+function quoteToggleShown(quoteId, quoteClassName, forceHide) {
   /* quote-collapsed = still available, quote-hidden = invisible */
   quoteClassName = quoteClassName || 'quote-collapsed';
-
-  var elem = document.getElementById('quote-id-' + quoteId);
+  forceHide = forceHide !== undefined ? forceHide : false;
+  
+  if (typeof(quoteId) == "object") {
+    var elem = quoteId;
+    quoteId = elem.dataset["quoteId"];
+  } else {
+    var elem = document.getElementById('quote-id-' + quoteId);
+  }
 
   if (elem == null) {
     throw Error('Quote with ID ' + quoteId + ' does not exist.');
   }
 
-  if (elem.className.indexOf(quoteClassName) !== -1) {
+  if (!forceHide && elem.className.indexOf(quoteClassName) !== -1) {
     elem.className = elem.className.replace(quoteClassName, '')
     elem.getElementsByClassName('btn-hide')[0].text = 'hide';
   } else {
@@ -65,13 +72,54 @@ function handleGetRequest(query) {
 }
 
 function showAllByAuthor(author) {
+  author = author.replace('_', ' ').toLowerCase();
+  var quotes = document.getElementsByClassName('quote-author');
 
+  for (var i = 0, item; i < quotes.length; i++) {
+    item = quotes[i];
+    if (item.innerHTML.toLowerCase().indexOf(author) === -1) {
+      quoteToggleShown(findQuoteArticleFromInner(item), 'quote-hidden', true);
+    }
+  }
 }
 
 function showAllThatMatchSearch(search) {
+  var quotes = document.getElementsByClassName('quote-text');
 
+  for (var i = 0, item; i < quotes.length; i++) {
+    item = quotes[i];
+    if (item.innerHTML.toLowerCase().indexOf(search.toLowerCase()) == -1) {
+      quoteToggleShown(findQuoteArticleFromInner(item), 'quote-hidden', true);
+    }
+  }
 }
 
 function showAllQuotesByIds(quoteIds) {
+  var quotes = document.getElementsByClassName('quote-wrapper');
+  for (var i = 0, item; i < quotes.length; i++) {
+    item = quotes[i];
+    quoteToggleShown(item, 'quote-hidden', true);
+  }
 
+  for (var i = 0, item; i < quoteIds.length; i++) {
+    item = parseInt(quoteIds[i]);
+
+    quoteToggleShown(item, 'quote-hidden');
+  }
+}
+
+function findQuoteArticleFromInner(elem, recursionLevel) {
+  if (recursionLevel == undefined) { 
+    recursionLevel = 25; 
+  }
+
+  if (recursionLevel == 0) {
+    throw Error("Error: findQuoteArticleFromInner: too much recursion.");
+  }
+
+  if (elem.className.indexOf("quote-wrapper") !== -1 && elem.dataset["quoteId"] != undefined) {
+    return elem;
+  }
+
+  return findQuoteArticleFromInner(elem.parentElement, --recursionLevel);
 }
